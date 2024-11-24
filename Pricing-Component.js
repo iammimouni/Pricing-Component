af6944ff5f27a51568c4eb16bd858b19d6ed549b
immimouni <your.email@example.com>
2024-11-24 14:29:24 +0100

    files Backend Project

---
+++ Pricing-Component.js
@@ -0,0 +1,9 @@
+document.getElementById("pricing-toggle").addEventListener("change", (event) => {
+    const isYearly = event.target.checked;
+    document.querySelectorAll(".price-value").forEach((priceElement) => {
+        const price = isYearly
+            ? priceElement.getAttribute("data-year")
+            : priceElement.getAttribute("data-month");
+        priceElement.textContent = price;
+    });
+});
