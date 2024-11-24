dd8c238d0e8eef68736c73bfb278b4a74e324dc1
immimouni <your.email@example.com>
2024-11-24 14:58:22 +0100

    files Frontend

--- Pricing-Component.js
+++ Pricing-Component.js
@@ -1,9 +1,9 @@
 document.getElementById("pricing-toggle").addEventListener("change", (event) => {
     const isYearly = event.target.checked;
     document.querySelectorAll(".price-value").forEach((priceElement) => {
-        const price = isYearly
-            ? priceElement.getAttribute("data-year")
-            : priceElement.getAttribute("data-month");
+        const price = isYearly ?
+            priceElement.getAttribute("data-year") :
+            priceElement.getAttribute("data-month");
         priceElement.textContent = price;
     });
 });
